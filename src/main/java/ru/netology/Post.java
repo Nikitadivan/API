9f9643241556fd205d1a5829eb5b49886b55fbc4
Nikitadivan <sarapul-mebel@mail.ru>
2022-04-11 22:26:32 +0400

    first version

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+import ru.netology.FormDate;
+
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
